36d2b0abe0b964def2a112bdf925d5a2ca81a961
Louis Haddrell <louishaddrell@gmail.com>
2016-01-19 11:01:47 +0000

    Converted Yak attribute names to lowercase

--- yak.py
+++ yak.py
@@ -1,35 +1,35 @@
 
 class Yak(object):
     def __init__(self, json):
-        self.canDownVote = json['canDownVote']
-        self.canReply = json['canReply']
-        self.canReport = json['canReport']
-        self.canUpVote = json['canUpVote']
-        self.canVote = json['canVote']
+        self.can_downvote = json['canDownVote']
+        self.can_reply = json['canReply']
+        self.can_report = json['canReport']
+        self.can_upvote = json['canUpVote']
+        self.can_vote = json['canVote']
         self.comments = json['comments']
-        self.deliveryID = json['deliveryID']
+        self.delivery_id = json['deliveryID']
         self.gmt = json['gmt']
         self.handle = json['handle']
-        self.hidePin = json['hidePin']
+        self.hide_pin = json['hidePin']
         self.latitude = json['latitude']
         self.liked = json['liked']
         self.location = json['location']
-        self.locationDisplayStyle = json['locationDisplayStyle']
-        self.locationName = json['locationName']
+        self.location_display_style = json['locationDisplayStyle']
+        self.location_name = json['locationName']
         self.longitude = json['longitude']
         self.message = json['message']
-        self.messageID = json['messageID']
-        self.numberOfLikes = json['numberOfLikes']
-        self.posterID = json['posterID']
-        self.readOnly = json['readOnly']
+        self.message_id = json['messageID']
+        self.number_of_likes = json['numberOfLikes']
+        self.poster_id = json['posterID']
+        self.read_only = json['readOnly']
         self.reyaked = json['reyaked']
         self.score = json['score']
         self.time = json['time']
         self.type = json['type']
 
         # Image Yaks
-        self.expandInFeed = json.get('expandInFeed', 0)
-        self.imageHeight = json.get('imageHeight', 0)
-        self.imageWidth = json.get('imageWidth', 0)
-        self.thumbNailUrl = json.get('thumbNailUrl', None)
+        self.expand_in_feed = json.get('expandInFeed', 0)
+        self.image_height = json.get('imageHeight', 0)
+        self.image_width = json.get('imageWidth', 0)
+        self.thumbnail_url = json.get('thumbNailUrl', None)
         self.url = json.get('url', None)
