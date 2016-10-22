f98c8687ea9d6a217098b0f3938c7f59e7273072
Louis Haddrell <louishaddrell@gmail.com>
2016-10-22 02:29:23 +0100

    Updated Yakker's refresh URL and added additional userLat and userLong parameters

--- tests/test_yakker.py
+++ tests/test_yakker.py
@@ -5,8 +5,7 @@ from yikyakapi.yakker import *
 
 
 class TestSuite(unittest.TestCase):
-    @mock.patch('yikyakapi.yikyak.Yakker._request')
-    def test_construction(self, mock_request):
+    def test_construction(self):
         session = mock.Mock()
         data = {
             'myHerd': {},
@@ -16,17 +15,29 @@ class TestSuite(unittest.TestCase):
         }
 
         yakker = Yakker(session, data)
+        self.assertEqual(yakker.base_url, 'https://www.yikyak.com/api/v2/')
         self.assertEqual(yakker.session, session)
         self.assertEqual(yakker.herd, data['myHerd'])
         self.assertEqual(yakker.nickname, data['nickname'])
         self.assertEqual(yakker.userID, data['userID'])
         self.assertEqual(yakker.yakarma, data['yakarma'])
 
-    @mock.patch('yikyakapi.yikyak.Yakker._request')
-    def test_construction_defaults(self, mock_request):
+    def test_construction_defaults(self):
         session = mock.Mock()
         yakker = Yakker(session, {})
+        self.assertEqual(yakker.base_url, 'https://www.yikyak.com/api/v2/')
+        self.assertEqual(yakker.session, session)
         self.assertEqual(yakker.herd, None)
         self.assertEqual(yakker.nickname, None)
         self.assertEqual(yakker.userID, None)
         self.assertEqual(yakker.yakarma, 0)
+
+    @mock.patch('yikyakapi.yikyak.Yakker._request')
+    def test_refresh(self, mock_request):
+        mock_request.return_value = {}
+        yakker = Yakker(mock.Mock(), {})
+        yakker.refresh()
+
+        params = {'userLat': 0, 'userLong': 0}
+        url = 'https://www.yikyak.com/api/v2/yakker/init'
+        mock_request.assert_called_with('GET', url, params=params)
--- yikyakapi/yakker.py
+++ yikyakapi/yakker.py
@@ -3,6 +3,8 @@ from .web import WebObject
 
 class Yakker(WebObject):
     def __init__(self, session, data):
+        super().__init__()
+
         self.session = session
         self.herd = data.get('myHerd', None)
         self.nickname = data.get('nickname', None)
@@ -10,6 +12,10 @@ class Yakker(WebObject):
         self.yakarma = data.get('yakarma', 0)
 
     def refresh(self):
-        url = 'https://www.yikyak.com/api/proxy/v1/yakker'
-        data = self._request('GET', url)
-        self.__init__(self.session, data)
+        url = self.base_url + 'yakker/init'
+        params = {
+            'userLat': 0,
+            'userLong': 0,
+        }
+        json = self._request('GET', url, params=params)
+        self.__init__(self.session, json)
